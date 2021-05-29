f23ab34ce6471492b78f3895ee532116964be3f5
Édipo Maciel <edipo@auditore.com.br>
2021-05-28 23:21:52 -0300

    refactor: rename datacadastro and removes datasaida

--- src/main/java/br/com/ViniciusAlvesCandido/domain/Cliente.java
+++ src/main/java/br/com/ViniciusAlvesCandido/domain/Cliente.java
@@ -25,18 +25,15 @@ public class Cliente extends AbstractEntity<Long> {
 	@Column(nullable = false, columnDefinition = "DECIMAL(7,2) DEFAULT 0.00")
 	private BigDecimal salario;
 	
-	@Column(name = "data_entrada", nullable = false, columnDefinition = "DATE")
-	private LocalDate dataEntrada;
-	
-	@Column(name = "data_saida", nullable = false, columnDefinition = "DATE")
-	private LocalDate dataSaida;
+	@Column(name = "data_cadastro", nullable = false, columnDefinition = "DATE")
+	private LocalDate dataCadastro;
 	
 	@OneToOne
 	@JoinColumn(name = "endereco_id_fk")
 	private Endereco endereco;
 	
 	@ManyToOne
-	@JoinColumn(name = "cargo_id_fk")
+	@JoinColumn(name = "profissao_id_fk")
 	private Profissao profissao;
 	
 	
